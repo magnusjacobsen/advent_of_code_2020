9ba6b2a840e84181841631d9825a652c13649062
magnusjacobsen <magnusjacobsen@gmx.com>
2020-12-06 11:19:04 +0100

    compacted 06/first

--- 06/first.py
+++ 06/first.py
@@ -1,14 +1,2 @@
-with open('input', 'r') as file:
-    lines = file.readlines()
-    current = ''
-    count = 0
-
-    for line in lines:
-        line = line.strip()
-        if line == '':
-            count += len(''.join(set(current)))
-            current = ''
-        else:
-            current += line
-    count += len(''.join(set(current)))
-    print(count)
+import sys
+print(sum([len(''.join(set(x.replace('\n','')))) for x in sys.stdin.read().split('\n\n')]))
